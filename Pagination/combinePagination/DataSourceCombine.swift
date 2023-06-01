4da5d5e8a115c125f1942951eb04eb437557bcbc
Dardo Gonzalez <dardo.gonzalez@care.com>
2023-06-01 17:14:46 -0300

    Combine

--- Pagination/combinePagination/DataSourceCombine.swift
+++ Pagination/combinePagination/DataSourceCombine.swift
@@ -7,56 +7,46 @@ open class DataSourceCombine<T: Identifiable> {
     private let pageSize: Int
     private var totalCount = -1
     private var retrievedIds: [T.ID] = []
-    private var subscription: AnyCancellable?
-
+    
     internal func fetchRequest(after: Int, first: Int) -> AnyPublisher<DataSourcePage<T>, Error> {
         fatalError("Fetch method not implemented")
     }
-
+    
     init(pageSize: Int) {
         self.pageSize = pageSize
     }
-
+    
     func retrieveNextPage() -> AnyPublisher<[T], Error>  {
-        let subject = PassthroughSubject<[T], Error>()
-
         if !shouldRetrieveNextPage() {
-            DispatchQueue.main.async {
-                subject.send([])
-            }
-        } else {
-            subscription = fetchRequest(after: page - 2, first: pageSize)
-                .sink(
-                    receiveCompletion: { [weak self] result in
-                        if case .failure(let error) = result {
-                            subject.send(completion: .failure(error))
-                        } else {
-                            self?.page += 1
-                            self?.totalCount = 0
-                            subject.send([])
-                        }
-                    },
-                    receiveValue: { [weak self] dataSourcePage in
-                        self?.page += 1
-                        self?.totalCount = dataSourcePage.totalCount // totalCount may vary between subsequent calls because new items can be added in the BE
-                        let page = self?.processPage(page: dataSourcePage.page) ?? dataSourcePage.page
-                        subject.send(page)
-                    }
-                )
+            return createEmptyPublisher()
         }
         
-        return subject.eraseToAnyPublisher()
+        return fetchRequest(after: page - 2, first: pageSize)
+            .flatMap { [weak self] dataSourcePage in
+                Just(self?.processDataSource(dataSourcePage) ?? [])
+            }
+            .eraseToAnyPublisher()
     }
     
     private func shouldRetrieveNextPage() -> Bool {
         totalCount < 0 ? true : page * pageSize < totalCount
     }
 
+    func createEmptyPublisher() -> AnyPublisher<[T], Error> {
+        let subject = PassthroughSubject<[T], Error>()
+        DispatchQueue.main.async {
+            subject.send([])
+        }
+        return subject.eraseToAnyPublisher()
+    }
+    
     // If new items are added in the BE between subsequent calls, this can result in getting
     // again a previously obtained item in the next requested page. To avoid duplicating it
     // we check them with the already retrieved ids
-    private func processPage(page: [T]) -> [T] {
-        let newItems = page.filter { !retrievedIds.contains($0.id) }
+    func processDataSource(_ dataSourcePage: DataSourcePage<T>) -> [T] {
+        page += 1
+        totalCount = dataSourcePage.totalCount // totalCount may vary between subsequent calls because new items can be added in the BE
+        let newItems = dataSourcePage.page.filter { !retrievedIds.contains($0.id) }
         retrievedIds.append(contentsOf: newItems.map { $0.id} )
         return newItems
     }
